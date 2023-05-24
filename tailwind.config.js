91f204a6046900d72140d661a45ec3f484d4473e
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-24 17:58:31 +0700

    tugas 10

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      colors: {
+        'white': '#FFFBFF',
+        'brown': '#362417',
+        'black': '#92817A',
+        'pink': '#F1DABF',
+        'purple': '#92817A',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
+    },
+  }
